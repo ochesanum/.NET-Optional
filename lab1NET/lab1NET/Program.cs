d4dbbced8f606f0d61f925a0942ef87c7b8ac3ae
ochesanum <79115414+ochesanum@users.noreply.github.com>
2021-09-29 00:13:23 +0300

    update to lab1

--- lab1NET/lab1NET/Program.cs
+++ lab1NET/lab1NET/Program.cs
@@ -2,7 +2,11 @@
 
 namespace lab1NET
 {
-    class Manager
+    public abstract class Employee
+    {
+        public abstract String Salutations();
+    }
+    public class Manager : Employee
     {
         private long id;
         private String firstname;
@@ -27,26 +31,85 @@ namespace lab1NET
 
         public String GetFullName()
         {
-            return firstname + lastname;
+            return firstname + " " + lastname;
         }
 
-        public double IsActive()
+        public String IsActive()
         {
             if ((EndDate.Ticks - StartDate.Ticks) < 0)
-                return DateTime.MinValue.Ticks;
+                return "Inactive";
 
-            return (EndDate - StartDate).TotalDays;
+            return "Active";
 
         }
 
+        public override String Salutations()
+        {
+            return "Hello Manager";
+        }
+        
+    }
+
+    public class Arhitect : Employee
+    {
+        private long id;
+        private String firstname;
+        private String lastname;
+        private DateTime StartDate;
+        private DateTime EndDate;
+        private int salary;
+
+        public Arhitect()
+        {
+        }
+
+        public Arhitect(long id, string firstname, string lastname, DateTime startDate, DateTime endDate, int salary)
+        {
+            this.id = id;
+            this.firstname = firstname;
+            this.lastname = lastname;
+            StartDate = startDate;
+            EndDate = endDate;
+            this.salary = salary;
+        }
+        public String GetFullName()
+        {
+            return firstname + " " + lastname;
+        }
+
+        public String IsActive()
+        {
+            if ((EndDate.Ticks - StartDate.Ticks) < 0)
+                return "Not Active";
+
+            return "Active";
+
+        }
+        public override String Salutations()
+        {
+            return "Hello Arhitect";
+        }
+    }
+
+    public static class ExtensionExample
+    {
+        public static void ComputeSentece(String str)
+        {
+            string[] words = str.Split(" ");
+            foreach (var word in words)
+                Console.WriteLine(word);
+        }
     }
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Manager m = new Manager(1, "Firstname", "Lastname", new System.DateTime(2010,01,01), new System.DateTime(2020,12,31), 3000);
+            Manager m = new Manager(1, "John", "Silver", new System.DateTime(2010,01,01), new System.DateTime(2020,12,31), 3000);
             Console.WriteLine(m.GetFullName()+" " + m.IsActive());
+            Arhitect a = new Arhitect();
+            Console.WriteLine(m.Salutations()+ " " + a.Salutations());
+            ExtensionExample.ComputeSentece("The quick brown fox jumps over the lazy dog.");
         }
     }
 }
